57da9bc216dafde8c130f4962196a8f73b01c62f
Enayat Badru <Enayatbadru07@gmail.com>
2024-05-16 15:01:38 +0200

    my first commit

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import { initializeApp } from '@firebase/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDt2g4R_ZlO789Sv-22EEgbJyhSm6PrrHM",
+  authDomain: "fir-auth-dd136.firebaseapp.com",
+  projectId: "fir-auth-dd136",
+  storageBucket: "fir-auth-dd136.appspot.com",
+  messagingSenderId: "980938023858",
+  appId: "1:980938023858:web:4a564d576269934720faab",
+  measurementId: "G-NQC8BVRN1B"
+};
+
+
 
 export default function App() {
   return (
@@ -12,9 +27,46 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexGrow: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#f0f0f0',
+  },
+  authContainer: {
+    width: '80%',
+    maxWidth: 400,
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    borderRadius: 4,
+  },
+  buttonContainer: {
+    marginBottom: 16,
+  },
+  toggleText: {
+    color: '#3498db',
+    textAlign: 'center',
+  },
+  bottomContainer: {
+    marginTop: 20,
+  },
+  emailText: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
